124aba7c9d375214912fb9d0cefb99a147dec293
mrthnt <hanneton.martin@gmail.com>
2025-12-03 09:22:01 +0100

    test

--- .py
+++ .py
@@ -1,1 +1,11 @@
+import soundfile as sf
+from IPython.display import Audio
+import numpy as np
 
+# Lecture propre
+sound, rate = sf.read("sonogramme/sound.wav", dtype="int16")
+
+# Si tu veux un tableau numpy comme avec SciPy
+sound = np.asarray(sound)
+
+Audio(sound, rate=rate)
